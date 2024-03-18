0eac98c7876902db6123d7d6853181677fdaacb5
Gkmegh <118533859+Gkmegh@users.noreply.github.com>
2024-03-18 19:58:00 +0530

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
+import store from './store.js'
+import {Provider} from "react-redux"
+import Counter from './Counter.js'
+
 
 const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
+    <Provider store={store}>
+      <div>
+        <h1>Counter App</h1>
+        <Counter/>
+      </div>
+    </Provider>
   )
 }
 
---
+++ src/components/actions.js
@@ -0,0 +1,12 @@
+
+export const increase = ()=>{
+    return{
+        type: "INCREASE"
+    }
+}
+
+export const decreace = () =>{
+    return{
+        type : "DECREASE"
+    }
+}
